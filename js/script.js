b31cac2ad53a0935019d394a262d75212f4175fe
AlexeyG1991 <68025816+AlexeyG1991@users.noreply.github.com>
2021-07-10 11:42:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// ----------------------B--------------------------
+let menuIcon = document.querySelector('.menu-icon');
+let menuBody = document.querySelector('.menu-body');
+menuIcon.addEventListener('click', function () {
+    menuIcon.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+})
+
+
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+// Цикл по всем ссылкам
+for (let anchor of anchors) {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault() // Предотвратить стандартное поведение ссылок
+        // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+        const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+        // Плавная прокрутка до элемента с id = href у ссылки
+        document.querySelector(goto).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        })
+    })
+}
